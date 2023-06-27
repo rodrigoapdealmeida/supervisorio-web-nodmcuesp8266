fb244a53262f8083c38dfc5d3740b0299c7d9c97
Rodrigo Aparecido De Almeida <rodrigoap.dealmeida@gmail.com>
2023-06-27 19:39:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# supervisorio-web-nodmcuesp8266
+ 
---
+++ js/script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body"),
+        sidebar = body.querySelector(".sidebar"),
+        toggle = body.querySelector(".toggle"),
+        modeSwitch = body.querySelector(".toggle-switch"),
+        modeText = body.querySelector(".mode-text");
+
+        toggle.addEventListener('click', () =>{
+            sidebar.classList.toggle("close");
+        });
+
+        modeSwitch.addEventListener('click', () =>{
+            body.classList.toggle("dark");
+
+            if(body.classList.contains("dark")){
+                modeText.innerText = "Light Mode"
+            }else {
+                modeText.innerText = "Dark Mode"
+            }
+        });
+
+// Toggle Menu
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu");
+}
+        
